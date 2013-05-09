74eeb5ea59a378b40ada84f771dacc3f4c018506
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-05-09 00:34:15 -0700

    Original update

---
+++ AccSim/Classes/AccelerationInfo.m
@@ -0,0 +1,43 @@
+//
+//  AccelerationInfo.m
+//  AccSim
+//
+//  Created by Otto Chrons on 9/26/08.
+//  Copyright 2008 Enzymia Ltd.. All rights reserved.
+//
+
+#import "AccelerationInfo.h"
+
+
+@implementation AccelerationInfo
+
+@synthesize deviceID;
+@synthesize absTime;
+@synthesize x, y, z;
+
++ (AccelerationInfo*)createWithTimestamp:(NSTimeInterval)timeStamp X:(UIAccelerationValue)x Y:(UIAccelerationValue)y Z:(UIAccelerationValue)z
+{
+	AccelerationInfo *newInfo = [AccelerationInfo alloc];
+
+	newInfo.absTime = timeStamp;
+	newInfo.deviceID = [[UIDevice currentDevice] uniqueIdentifier];
+	newInfo.x = x;
+	newInfo.y = y;
+	newInfo.z = z;
+	
+	return newInfo;
+}
+
++ (AccelerationInfo*)createWithTimestamp:(NSTimeInterval)timeStamp X:(UIAccelerationValue)x Y:(UIAccelerationValue)y Z:(UIAccelerationValue)z deviceID:(NSString *)devID;
+{
+	AccelerationInfo *newInfo = [AccelerationInfo alloc];
+	newInfo.absTime = timeStamp;
+	newInfo.deviceID = devID;
+	newInfo.x = x;
+	newInfo.y = y;
+	newInfo.z = z;
+
+	return newInfo;
+}
+
+@end
